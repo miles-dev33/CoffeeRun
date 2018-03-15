0d820c71b26ce8dee2dac819b42fae70b0c12083
milesjmccloskey <milesjmccloskey@gmail.com>
2018-03-14 20:40:44 -0700

    uploading work for hw 4

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -1,4 +1,4 @@
-(function (window) {
+(function(window) {
   "use strict";
   var App = window.App || {};
   var $ = window.jQuery;
@@ -13,13 +13,13 @@
     }
   }
 
-  FormHandler.prototype.addSubmitHandler = function (fn) {
+  FormHandler.prototype.addSubmitHandler = function(fn) {
     console.log("Setting submit handler for form");
-    this.$formElement.on("submit", function (event) {
+    this.$formElement.on("submit", function(event) {
       event.preventDefault();
 
       var data = {};
-      $(this).serializeArray().forEach(function (item) {
+      $(this).serializeArray().forEach(function(item) {
         data[item.name] = item.value;
         console.log(item.name + " is " + item.value);
       });
@@ -27,6 +27,20 @@
       fn(data);
       this.reset();
       this.elements[0].focus();
+    });
+  };
+
+  FormHandler.prototype.addInputHandler = function(fn) {
+    console.log("Setting input handler for form");
+    this.$formElement.on("input", "[name=\"emailAddress\"]", function(event) {
+      var emailAddress = event.target.value;
+      var message = "";
+      if (fn(emailAddress)) {
+        event.target.setCustomValidity("");
+      } else {
+        message = emailAddress + " is not authorized email address!";
+        event.target.setCustomValidity(message);
+      }
     });
   };
 
